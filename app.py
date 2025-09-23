65f6a9efe2a8858aee5f9a27a0a8709f36530da9
WARIN <147617436+warinnnn0927@users.noreply.github.com>
2025-09-23 19:20:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello World')
+import streamlit as st
+
+#อาหารกับวัตถุดิบที่ใช้
+Pad_Thai = {"Flat rice noodles", "Eggs", "Garlic", "Shallots", "Dried shrimp", "Bean sprouts", "Chinese chives", "Lime"}
+Tom_Yum = {"Lemongrass", "Galangal", "Kaffir lime leaves", "Chili", "Fish sauce", "Lime", "Shrimp"}
+
+Food = [Pad_Thai, Tom_Yum]
+
+Ingredients = set.union(*Food)
+
+st.multiselect("What do you have in your fridge?", Ingredients)
